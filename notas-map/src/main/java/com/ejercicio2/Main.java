06c2fadcfa1b49733ed0e38ca49fba502ac0f5f8
RicardoArevaloB <160280340+RicardoArevaloB@users.noreply.github.com>
2025-02-21 16:25:02 -0500

    Notas con Map

---
+++ notas-map/src/main/java/com/ejercicio2/Main.java
@@ -0,0 +1,22 @@
+package com.ejercicio2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            Map<String, Double> estudiantes = new HashMap<>();
+
+            System.out.print("Ecribe el numero de estudiantes que vas a registrar: ");
+            String cantidadEstudiantes = sc.nextLine();
+            sc.nextLine();
+
+        }
+
+        
+
+    }
+}
